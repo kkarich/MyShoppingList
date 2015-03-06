73cce0e19b7a7fe04c1597dec93a3762b4759339
Keenan Karich <kkarich@yahoo.com>
2015-03-05 21:37:43 -0600

    updated db

--- config/env/development.js
+++ config/env/development.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	db: {
-		uri: 'mongodb://localhost/mean-dev',
+		uri: 'mongodb://heroku_app32408610:6ve7q0t9ki23cfuo7dvciubbre@ds063170.mongolab.com:63170/heroku_app32408610',
 		options: {
 			user: '',
 			pass: ''
